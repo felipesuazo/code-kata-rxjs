95d9fd06136e2a8c76b4dfc31615e740dc0de38d
Felipe Suazo <fesuami@gmail.com>
2019-03-20 09:04:43 -0300

    Added example 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Subject, BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'code-kata-rxjs';
+
+  ngOnInit() {
+    const subject = new BehaviorSubject('Rankmi');
+
+    const observer1 = subject.subscribe(
+      data => console.log(data),
+      error => this.handleError(error),
+      () => console.log('observer1 completed')
+    )
+
+    subject.next('Hello');
+
+    const observer2 = subject.subscribe(
+      data => console.log(data),
+      error => this.handleError(error),
+      () => console.log('observer2 completed')
+    )
+
+    subject.next('World!');
+    subject.complete();
+  }
+
+  handleError(error) {
+    console.error(error);
+  }
 }
