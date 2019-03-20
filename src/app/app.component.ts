89b5a8be85db4e1a7a66cf564a436a75a70862bc
Felipe Suazo <fesuami@gmail.com>
2019-03-20 14:49:53 -0300

    Example 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Subject, BehaviorSubject, timer, interval } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { switchMap, tap, map } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -11,10 +12,12 @@ export class AppComponent implements OnInit {
   title = 'code-kata-rxjs';
 
   ngOnInit() {
-    const source = timer(0, 5000);
+    const source = of(1,2,3,4,5);
 
     const example = source.pipe(
-      switchMap(() => interval(500))
+      tap(value => console.log(`Before map ${value}`)),
+      map(value => value + 10),
+      tap(value => console.log(`After map ${value}`))
     );
 
     const subscribe = example.subscribe(value => console.log(value));
